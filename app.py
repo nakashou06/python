8c3b9bbc29c6ff1313b0d2b1bc7c94b80c8b7d55
nakashou06 <81433336+nakashou06@users.noreply.github.com>
2021-04-17 14:25:14 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+#20210414
+#kdbもどきもどき
+#from flask import Flask, render_template, request, jsonify #追加
+
+#app = Flask(__name__)
+#app.config['JSON_AS_ASCII'] = False #日本語文字化け対策
+#app.config["JSON_SORT_KEYS"] = False #ソートをそのまま
+
+#@app.route('/hello')
+#def hello():
+#    data = [
+#        {"name":"山田"},
+#        {"age":30}
+#    ]
+#    return jsonify({
+#            'status':'OK',
+#            'data':data
+#        })
+
+## おまじない
+#if __name__ == "__main__":
+#    app.run(debug=True)
